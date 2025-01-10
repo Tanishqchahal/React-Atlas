fc2cf636fd70f3121fc7c06776ccce038e68a811
Tanishq Chahal <chahat5@mcmaster.ca>
2025-01-09 22:05:42 -0500

    create footer

--- worldAtlas/src/components/UI/Footer.jsx
+++ worldAtlas/src/components/UI/Footer.jsx
@@ -1,9 +1,77 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import footerData from "../../api/footerData.json";
+import { MdPlace } from "react-icons/md";
+import { IoCallSharp } from "react-icons/io5";
+import { TbMailPlus } from "react-icons/tb";
 
 function Footer() {
+  const footerIcon = {
+    MdPlace: <MdPlace />,
+    IoCallSharp: <IoCallSharp />,
+    TbMailPlus: <TbMailPlus />,
+  };
+
   return (
-    <h1>Footer</h1>
-  )
+    <footer className="footer-section">
+      <div className="container grid grid-three-cols">
+        {footerData.map((curData, index) => {
+          const { icon, title, details } = curData;
+          return (
+            <div className="footer-contact" key={index}>
+              <div className="icon">{footerIcon[icon]}</div>
+              <div className="footer-contact-text">
+                <p>{title}</p>
+                <p>{details}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="copyright-area">
+        <div className="container">
+          <div className="grid grid-two-cols">
+            <div className="copyright-text">
+              <p>
+                Copyright &copy; 2024, All Right Reserved
+                <NavLink to="https://tanishqchahal.streamlit.app/" target="_blank">
+                  Tanishq Chahal
+                </NavLink>
+              </p>
+            </div>
+
+            <div className="footer-menu">
+              <ul>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+
+                <li>
+                  <NavLink
+                    to="https://www.linkedin.com/in/tanishqchahal/"
+                    target="_blank"
+                  >
+                    Social
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="https://github.com/Tanishqchahal/React-Atlas"
+                    target="_blank"
+                  >
+                    Source Code
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
