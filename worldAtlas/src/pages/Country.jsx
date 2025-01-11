1ab612be66a195f10872a344df7eb0da371a22ce
Tanishq Chahal <chahat5@mcmaster.ca>
2025-01-10 21:59:23 -0500

    add search and filter function

--- worldAtlas/src/pages/Country.jsx
+++ worldAtlas/src/pages/Country.jsx
@@ -2,10 +2,13 @@ import React, { useEffect, useState, useTransition } from "react";
 import { getCountryData } from "../api/countryApi";
 import { Loader } from "../components/UI/Loader";
 import CountryCard from "../components/Layout/CountryCard";
+import SearchFilter from "../components/UI/SearchFilter";
 
 function Country() {
   const [isPending, startTransition] = useTransition();
   const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     startTransition(async () => {
@@ -15,11 +18,36 @@ function Country() {
   }, []);
 
   if (isPending) return <Loader />;
+
+  function searchCountry(country) {
+    if (search) {
+      return country.name.common.toLowerCase().includes(search);
+    }
+    return country;
+  }
+
+  function filterRegion(country) {
+    if (filter === "all") {
+      return country;
+    }
+    return country.region === filter;
+  }
+
+  const filterCountries = countries.filter(
+    (country) => searchCountry(country) && filterRegion(country)
+  );
+
   return (
     <>
       <section className="country-section">
+        <SearchFilter
+          search={search}
+          setSearch={setSearch}
+          filter={filter}
+          setFilter={setFilter}
+        />
         <ul className="grid grid-four-cols">
-          {countries.map((country, index) => {
+          {filterCountries.map((country, index) => {
             return <CountryCard country={country} key={index} />;
           })}
         </ul>
